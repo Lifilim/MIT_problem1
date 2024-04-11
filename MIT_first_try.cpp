807b04d42c6dbd6ebd32631e2ebb1219402bfa49
Lifilim <d1804a@gmail.com>
2024-04-11 22:19:43 +0400

    print the first n elements of the Fibonacci sequence

--- MIT_first_try.cpp
+++ MIT_first_try.cpp
@@ -1,17 +1,25 @@
 ﻿#include <iostream>
+#include <vector>
 
 using namespace std;
 
+vector<int> fib_history(int(1e9) + 7, -1);
+
 int fib(int n) {
-    if (n == 1) return 0;
-    if (n == 2) return 1;
-    return fib(n - 1) + fib(n - 2);
+    if (fib_history[n] == -1) fib_history[n] = fib(n - 1) + fib(n - 2);
+    return fib_history[n];
 }
 
 int main() {
     cout << "Hello, World!\n";
+
+    fib_history[1] = 0;
+    fib_history[2] = 1;
+
     int n;
     cout << "Enter n: ";
     cin >> n;
-    cout << fib(n);
+    fib(n);
+    for (int i = 1; i <= n; ++i)
+    cout << fib_history[i] << '\n';
 }
