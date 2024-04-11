088c5df4c28ab8d833eb4061b3cd1d6558a29083
Lifilim <d1804a@gmail.com>
2024-04-11 20:58:13 +0400

    Fibonacci sequence added (1, 1, 2, ..)

--- MIT_first_try.cpp
+++ MIT_first_try.cpp
@@ -2,6 +2,16 @@
 
 using namespace std;
 
+int fib(int n) {
+    if (n == 1) return 1;
+    if (n == 2) return 1;
+    return fib(n - 1) + fib(n - 2);
+}
+
 int main() {
     cout << "Hello, World!\n";
+    int n;
+    cout << "Enter n: ";
+    cin >> n;
+    cout << fib(n);
 }
